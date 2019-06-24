957a3b3a01aa96296be448f709287da3c965dbaa
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-23 22:31:31 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const cool = require('cool-ascii-faces')
     const express = require('express')
     const path = require('path')
 const PORT = process.env.PORT || 5000
+    
 
     express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -42,6 +43,8 @@ const PORT = process.env.PORT || 5000
                 res.send("Error " + err);
             }
         })
+    
+    .get('/', (req, res) => res.send('Hello World!'))
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
