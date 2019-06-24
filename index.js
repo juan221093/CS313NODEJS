f3a8ada2dad912c21b091d43be0d4e03534cba86
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-23 22:17:01 -0600

    Added /db

--- index.js
+++ index.js
@@ -30,17 +30,18 @@ const PORT = process.env.PORT || 5000
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
     .get('/cool', (req, res) => res.send(cool()))
+    .get('/db', async (req, res) => {
+            try {
+                const client = await pool.connect()
+                const result = await client.query('SELECT * FROM test_table');
+                const results = { 'results': (result) ? result.rows : null};
+                res.render('pages/db', results );
+                client.release();
+            } catch (err) {
+                console.error(err);
+                res.send("Error " + err);
+            }
+        })
+
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-    .get('/db', async (req, res) => {
-	    try {
-		const client = await pool.connect()
-		const result = await client.query('SELECT * FROM test_table');
-		const results = { 'results': (result) ? result.rows : null};
-		res.render('pages/db', results );
-		client.release();
-	    } catch (err) {
-		console.error(err);
-		res.send("Error " + err);
-	    }
-	})
