a081538eb8533b48e8dad7c9c67cdd4066dfce6e
Juan Alvarez <juanitoalvarez221093@gmail.com>
2019-06-23 22:14:48 -0600

    Added /db

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
 
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+    });
+
+
+
+ ////////
+
 const cool = require('cool-ascii-faces')
     const express = require('express')
     const path = require('path')
@@ -21,3 +31,16 @@ const PORT = process.env.PORT || 5000
     .get('/', (req, res) => res.render('pages/index'))
     .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+    .get('/db', async (req, res) => {
+	    try {
+		const client = await pool.connect()
+		const result = await client.query('SELECT * FROM test_table');
+		const results = { 'results': (result) ? result.rows : null};
+		res.render('pages/db', results );
+		client.release();
+	    } catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	    }
+	})
